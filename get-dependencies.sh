eeda9f0a133449ad8b3cb0f2d1a29265c70d304c
Link4Electronics <link4electronics@gmail.com>
2026-02-27 04:46:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,9 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    qt6-base       \
+    qt5-multimedia
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -16,11 +18,24 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Building CLK..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/ArnoAnsems/CatacombGL"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of CLK..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone "$REPO" ./CLK
+else
+	echo "Making stable build of CLK..."
+	VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+	git clone --branch "$VERSION" --single-branch "$REPO" ./CLK
+fi
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./CLK/OSBindings/Qt
+qmake
+make
+mv -v clksignal ../../../AppDir/bin
