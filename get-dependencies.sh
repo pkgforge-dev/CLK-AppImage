d18d2fc49c10d45c4befde8af70e63bf7f80f0df
Link4Electronics <link4electronics@gmail.com>
2026-02-27 05:04:39 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,7 +28,7 @@ if [ "${DEVEL_RELEASE-}" = 1 ]; then
     git clone "$REPO" ./CLK
 else
 	echo "Making stable build of CLK..."
-	VERSION=$(git ls-remote --tags --sort="refname" "$REPO" | tail -n1 | sed 's|.*/||; s/\^{}//')
+	VERSION=$(git ls-remote --tags --refs --sort="refname" "$REPO" | tail -n1 | sed 's|.*/||')
 	git clone --branch "$VERSION" --single-branch "$REPO" ./CLK
 fi
 echo "$VERSION" > ~/version
