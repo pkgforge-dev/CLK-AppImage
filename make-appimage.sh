d319600b96c89e871db5907fd1c2453f0d9a65fc
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:36:22 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,6 @@ export QT_DIR=qt6
 # Deploy dependencies
 quick-sharun ./AppDir/bin/clksignal
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
