841e26bf88c188ab590ed7b749bd3e70d382fa18
Link4Electronics <link4electronics@gmail.com>
2026-08-19 02:03:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# If the application needs to be manually built that has to be done down here
 echo "Building CLK..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/TomHarte/CLK"
